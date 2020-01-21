9606602a8d8f3b25f48caa9ec2e537467e886e26
harislapo <harislapo@gmail.com>
2020-01-21 20:35:13 +0100

    AngularJS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,78 @@
-var id = 0;
+
+var app = angular.module('myApp', [])
+    .controller('kontroler', function($scope){
+        $scope.user = { 
+            name: 'Mika', 
+            lastName: 'Mikic', 
+            adresa: 'Bosanska bb',
+            slika: 'https://cdn.vox-cdn.com/thumbor/0n6dqQfk9MuOBSiM39Pog2Bw39Y=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19341372/microsoftedgenewlogo.jpg'};
+    });
+
+
+
+
+
+
+
+
+
+
+/* drugi zadatak */
+/* var todos = [];
+
+function addTodo() {
+    var todo = {
+        name: document.getElementById('todo').value,
+        show: function (i) {
+            return `
+            <div>
+            <input type="checkbox" id="check">
+            <span>${todo.name}</span>
+            <button onclick="deleteTodo(${i})">Delete</button>
+            </div>
+            `
+        }
+    };
+    todos.push(todo);
+    document.getElementById('todo').value = ' ';
+    showAll();
+    console.log(todos);
+}
+
+function showAll() {
+    document.getElementById('todos').innerHTML = ' ';
+    for (let i = 0; i < todos.length; i++) {
+        document.getElementById('todos').innerHTML += todos[i].show(i);
+    }
+}
+
+function addOnEnter(evt){
+    if(evt.keyCode == 13){
+        addTodo();
+    }
+}
+
+var deleteAll = function () {
+    todos = [];
+    showAll();
+    console.log(todos);
+}
+
+function deleteTodo(index) {
+    console.log('Delete todo...' + index);
+    todos.splice(index, 1);
+    showAll();
+}
+
+
+
+
+
+
+
+ */
+/* prvi zadatak */
+/* var id = 0;
 var cars = [];
 
 function saveCar() {
@@ -52,4 +126,4 @@ function prikazi() {
         resItem.appendChild(h3);
         res.appendChild(resItem);
     }
-}
+} */
